e52c10bf2aae65f15281134944b6bc4da419c47f
Amy Dripchak <rejuvenatingserendipity@gmail.com>
2023-09-01 18:15:31 -0600

    update

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -2,17 +2,37 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
 
   erb :home
 end
 
-get('/chapters/1') do
-  @title = "Chapter 1"
+# get('/chapters/1') do
+#   @title = "Chapter 1"
+#   @contents = File.readlines("data/toc.txt")
+#   @chapter = File.readlines('data/chp1.txt')
+
+#   erb :chapter
+# end
+
+get "/chapters/:number" do
   @contents = File.readlines("data/toc.txt")
-  @chapter = File.readlines('data/chp1.txt')
+
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+  @title = "Chapter #{number}: #{chapter_name}"
+
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
+end
+
+get "/show/:name" do
+  params[:name]
 end
